ed969e95718ba6269431ca7bd4cb22e03e0aabfc
Nahum Santana <nahumsantanam@gmail.com>
2023-08-06 20:27:15 -0400

    fix: production procfile

--- backend/config/production.py
+++ backend/config/production.py
@@ -1,12 +1,31 @@
-from os import environ
-from backend.config.wsgi import * # noqa
+from backend.config.settings import * # noqa
+
+DEBUG = False
+
+
+# SECURITY
+# ------------------------------------------------------------------------------ 
+CORS_ALLOWED_ORIGINS = []
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = []
 ALLOWED_HOSTS = []
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://*.railway.app'
+ALLOWED_HOSTS = [
+    "joblinker-production.up.railway.app",
+    "jolinker.com"
 ]
 
-DEBUG = False
+CSRF_TRUSTED_ORIGINS = [
+    "https://joblinker-production.up.railway.app",
+    "jolinker.com"
+]
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+# STATIC
+# ------------------------
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.config.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.config.production")
+
+ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent
+sys.path.append(str(ROOT_DIR / "feline"))
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=str(ROOT_DIR / "staticfiles"))
+application.add_files(str(ROOT_DIR / "staticfiles"), prefix="staticfiles/")
