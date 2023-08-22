31a8f5fe3fd69a4d752610cbb33a55ab93b738bf
Nahum Santana <nahumsantanam@gmail.com>
2023-08-22 19:10:17 -0400

    fix: company details fixed

---
+++ frontend/components/page/company/details.server.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <a class="company-heading" href="{{ company.website }}?ref=www.joblinker.site&source=www.joblinker.site">
+            <div class="company--logo big">
+                <img :src="'{{ company.logo.url }}'" alt="{{ company.name }} logo">
+            </div>
+
+            <div class="inner">
+                <h1>{{ company.name }}</h1>
+                <cite>{{ company.website }}</cite>
+            </div>
+        </a>
+
+        <h2>About</h2>
+        <div class="description">
+            {{ company.description | safe }}
+        </div>
+    </div>
+</template>
