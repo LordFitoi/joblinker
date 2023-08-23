a4f2cff6488e496f35cf9cf3fb90c098854e607b
Nahum Santana <nahumsantanam@gmail.com>
2023-08-22 20:47:42 -0400

    fix: company seo

--- frontend/pages/companies/[company].vue
+++ frontend/pages/companies/[company].vue
@@ -1,22 +1,26 @@
 <template>
     <NuxtLayout name="content" class="company--page">
-        <PageCompanyDetails :company="route.params.company" />
+        <PageCompanyDetails :company="$route.params.company" />
     
         <PageAd adKey="b041a54588d9c42a335ae5bb9b246885" width="728" height="90" class="horizontal margin-bottom"/>
     
         <h2>Related jobs</h2>
-        <CardsTableJobpost :company="route.params.company" />
+        <CardsTableJobpost :company="$route.params.company" />
     </NuxtLayout>
 </template>
 <script>
 export default {
-    meta: {
-        script: [{ src: "~/assets/ads.js" }],
-    },
     setup() {
-        return {
-            route: useRoute()
+        if (process.server) {
+            useSeoMeta({
+                title: '{{ company.name }} - Joblinker Site',
+                ogTitle: '{{ company.name }} - Joblinker Site',
+                description: '{{ company.description | striptags | truncatechars:200 }}',
+                ogDescription: '{{ company.description | striptags | truncatechars:200 }}',
+                ogImage: '{{ company.logo.url }}',
+                twitterCard: 'summary_large_image',
+            });
         }
-    },
+    }
 }
 </script>
